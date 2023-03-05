3d9e3d19c375c11c1b5456f0aaf1b527f351259f
Samiur Khan <samiurkh1n@gmail.com>
2023-03-04 19:11:14 -0500

    style guide nit: numeric, rng

--- src/numeric.cpp
+++ src/numeric.cpp
@@ -10,38 +10,38 @@
 
 // TODO: extended Euclidean for RSA
 int gcd(int a, int b) {
-	if (a == 0) {
-		return b;
-	}
-	return gcd(b % a, a);
+  if (a == 0) {
+    return b;
+  }
+  return gcd(b % a, a);
 }
 
 // NOTE: a/g*b is preferred over a*b/g to prevent overflow
 int lcm(int a, int b) {
-	return a / gcd(a, b) * b;
+  return a / gcd(a, b) * b;
 }
 
 template <typename Iterator>
 void PrintContainerLn(std::string_view s, Iterator b, Iterator e) {
-	for (std::cout << s << ": "; b != e; std::cout << *b++ << " ") {}
-	std::cout << "\n";
+  for (std::cout << s << ": "; b != e; std::cout << *b++ << " ") {}
+  std::cout << "\n";
 }
 
 int main() {
   
-	int p = 2 * 4 * 5;
-	int q = 2 * 4 * 6;
-	assert(2 * 4 == std::gcd(p, q));
-	assert(2 * 4 == gcd(p, q));
+  int p = 2 * 4 * 5;
+  int q = 2 * 4 * 6;
+  assert(2 * 4 == std::gcd(p, q));
+  assert(2 * 4 == gcd(p, q));
 
-	p = 2 * 3;
-	q = 2 * 5;
-	assert(2 * 3 * 5 == std::lcm(p, q));
-	assert(2 * 3 * 5 == lcm(p, q));
+  p = 2 * 3;
+  q = 2 * 5;
+  assert(2 * 3 * 5 == std::lcm(p, q));
+  assert(2 * 3 * 5 == lcm(p, q));
 
-	std::vector<int> v(5, 0);
-	PrintContainerLn("v (initial)", v.begin(), v.end());
-	std::iota(v.begin(), v.end(), -1);
-	PrintContainerLn("v (iota(-1))", v.begin(), v.end());
-	return 0;
+  std::vector<int> v(5, 0);
+  PrintContainerLn("v (initial)", v.begin(), v.end());
+  std::iota(v.begin(), v.end(), -1);
+  PrintContainerLn("v (iota(-1))", v.begin(), v.end());
+  return 0;
 }
--- src/rng.cpp
+++ src/rng.cpp
@@ -11,49 +11,49 @@
 
 int main() {
 
-	std::mt19937 gen32;
-	std::mt19937_64 gen64;
+  std::mt19937 gen32;
+  std::mt19937_64 gen64;
 
-	for (int n = 1; n != 10000; gen32(), gen64(), ++n) {}
+  for (int n = 1; n != 10000; gen32(), gen64(), ++n) {}
 
-	assert(gen32() == 4123659995);
-	assert(gen64() == 9981545732273789042);
+  assert(gen32() == 4123659995);
+  assert(gen64() == 9981545732273789042);
 
-	std::random_device rd{};
-	std::mt19937 gen{rd()};
+  std::random_device rd{};
+  std::mt19937 gen{rd()};
 
-	// std::shuffle numbers with random generator
-	std::vector<int> l(10);
-	std::iota(l.begin(), l.end(), -5);
+  // std::shuffle numbers with random generator
+  std::vector<int> l(10);
+  std::iota(l.begin(), l.end(), -5);
 
-	std::cout << "Pre shuffle: ";
-	for(auto i : l) std::cout << i << " ";
-	std::cout << "\n";
+  std::cout << "Pre shuffle: ";
+  for(auto i : l) std::cout << i << " ";
+  std::cout << "\n";
 
-	std::shuffle(l.begin(), l.end(), gen);
+  std::shuffle(l.begin(), l.end(), gen);
 
-	std::cout << "Post shuffle: ";
-	for(auto i : l) std::cout << i << " ";
-	std::cout << "\n";
+  std::cout << "Post shuffle: ";
+  for(auto i : l) std::cout << i << " ";
+  std::cout << "\n";
 
-	// Simulate a 6 sided die with uniform distribution
-	std::uniform_int_distribution<> die(1, 6);
-	std::cout << "10 die rolls: ";
-	for (int n = 0; n != 10; ++n) std::cout << die(gen) << " ";
-	std::cout << "\n";
+  // Simulate a 6 sided die with uniform distribution
+  std::uniform_int_distribution<> die(1, 6);
+  std::cout << "10 die rolls: ";
+  for (int n = 0; n != 10; ++n) std::cout << die(gen) << " ";
+  std::cout << "\n";
 
-	// Normal distribution
-	std::normal_distribution<> d{5, 2};
+  // Normal distribution
+  std::normal_distribution<> d{5, 2};
 
-	std::map<int, int> hist;
-	for (int n = 0; n != 10000; ++n) {
-		++hist[std::round(d(gen))];
-	}
+  std::map<int, int> hist;
+  for (int n = 0; n != 10000; ++n) {
+    ++hist[std::round(d(gen))];
+  }
 
-	for (auto [x, y] : hist) {
-		std::cout << std::setw(2) << x << ' ' << std::string(y/200, '*') << "\n";
-	}
+  for (auto [x, y] : hist) {
+    std::cout << std::setw(2) << x << ' ' << std::string(y/200, '*') << "\n";
+  }
 
-	return 0;
+  return 0;
 
 }
